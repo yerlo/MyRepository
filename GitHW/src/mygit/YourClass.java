0d05da3c9578f5e7400ffd8daca4cab2cc65b4f4
서예은 <@192.168.200.124>
2023-12-20 04:28:10 +0900

    change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -3,6 +3,7 @@ package mygit;
 public class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.println("Hello!" + toWhom);
+		System.out.println("Your change in master branch");
 	}
 
 }
